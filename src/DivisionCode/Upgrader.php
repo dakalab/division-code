7e88d65935a1d504d10669ed9b9d9700ced4ad05
Hyper <hyper.jiang@lalamove.com>
2020-07-31 14:07:22 +0800

    UPT: update division codes to May 2020

--- src/DivisionCode/Upgrader.php
+++ src/DivisionCode/Upgrader.php
@@ -45,8 +45,10 @@ class Upgrader extends DivisionCode
         }
 
         // need to redirect
-        if (preg_match('/http:\/\/www\.mca\.gov\.cn\/article\/sj\/xzqh\/.+\.html/U', $html, $m)) {
+        if (preg_match('/http:\/\/www\.mca\.gov\.cn(\/)+article\/sj\/xzqh\/.+\.html/U', $html, $m)) {
             $html = file_get_contents($m[0]);
+        } else {
+            throw new \LengthException('Fail to parse redirect link');
         }
 
         if (empty($html)) {
